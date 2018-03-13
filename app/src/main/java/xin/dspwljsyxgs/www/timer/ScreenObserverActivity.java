d7d82989bccf245d97e3be9b48ab36ce7cdb8d50
TYZ <tyzjslyg@126.com>
2018-03-13 12:10:49 +0800

    Nothing important

--- app/src/main/java/xin/dspwljsyxgs/www/timer/ScreenObserverActivity.java
+++ app/src/main/java/xin/dspwljsyxgs/www/timer/ScreenObserverActivity.java
@@ -8,7 +8,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.util.*;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
 
 import xin.dspwljsyxgs.www.timer.ScreenObserver.ScreenStateListener;
 
@@ -16,12 +24,32 @@ public class ScreenObserverActivity extends Activity {
     private String TAG = "ScreenObserverActivity";
     private ScreenObserver mScreenObserver;
     private Vibrator vibrator=null;
+    private int label=0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(ScreenObserverActivity.this,"Welcome!",Toast.LENGTH_SHORT).show();
         vibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+
+
+
+        Button button1=(Button) findViewById(R.id.button1);
+        Button button2=(Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                label=1;
+                Toast.makeText(ScreenObserverActivity.this,"设置成功",Toast.LENGTH_SHORT).show();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                label=0;
+                Toast.makeText(ScreenObserverActivity.this,"设置成功",Toast.LENGTH_SHORT).show();
+            }
+        });
         mScreenObserver = new ScreenObserver(this);
         mScreenObserver.requestScreenStateUpdate(new ScreenStateListener() {
             @Override
@@ -43,15 +71,16 @@ public class ScreenObserverActivity extends Activity {
     }
 
     private void doSomethingOnScreenOff() {
+            if (label == 1) {
+                try {
 
-            try{
-                Thread.sleep(15000);
-            }
-            catch (Exception e){
+                        Thread.sleep(5000);
+                } catch (Exception e) {
 
+                }
+                Log.i(TAG, "Screen is off");
+                vibrator.vibrate(new long[]{100, 100}, 0);
             }
-            Log.i(TAG, "Screen is off");
-            vibrator.vibrate(new long[]{100, 100}, 0);
     }
 
     @Override
